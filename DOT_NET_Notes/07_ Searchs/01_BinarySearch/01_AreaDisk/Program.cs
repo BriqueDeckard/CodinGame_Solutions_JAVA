1b34b7dfbb19f508a1e5505d13a87c6bdeb8ea34
Brique Deckard <pierre.4ntoine@outlook.fr>
2021-10-08 16:43:58 +0200

    update

--- DOT_NET_Notes/07_ Searchs/01_BinarySearch/01_AreaDisk/Program.cs
+++ DOT_NET_Notes/07_ Searchs/01_BinarySearch/01_AreaDisk/Program.cs
@@ -6,7 +6,22 @@ namespace _01_AreaDisk
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int ray;
+            float
+                area,
+                pi = 3.14F;
+            Console.WriteLine("Entrez le rayon du cercle : ");
+            var temp = Console.ReadLine();
+            try
+            {
+                ray = Convert.ToInt32(temp);
+            }
+            catch (System.Exception e)
+            {
+                throw e;
+            }
+            area = pi * ray * ray;
+            Console.WriteLine("La surface du cercle est de  : " + area);
         }
     }
 }
