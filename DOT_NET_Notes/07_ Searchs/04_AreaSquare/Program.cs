57ee9b84ea4a2147b97129edf05f83ec3ae3d91e
Brique Deckard <pierre.4ntoine@outlook.fr>
2021-10-09 09:57:02 +0200

    update

---
+++ DOT_NET_Notes/07_ Searchs/04_AreaSquare/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace _04_AreaSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez la longueur du côté");
+            int side = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(CalculateSquareArea(side));
+        }
+
+        private static string CalculateSquareArea(int side)
+        {
+            var area = side * side;
+            StringBuilder sb = new StringBuilder("" + area);
+            return sb.ToString();
+        }
+    }
+}
