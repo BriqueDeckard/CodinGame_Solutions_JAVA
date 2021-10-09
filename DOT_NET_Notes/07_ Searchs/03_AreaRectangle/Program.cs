013f38401eebf06f604c4d133df3800d098c1cdf
Brique Deckard <pierre.4ntoine@outlook.fr>
2021-10-09 09:31:52 +0200

    update

---
+++ DOT_NET_Notes/07_ Searchs/03_AreaRectangle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace _03_AreaRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez la longueur: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Entrez la hauteur: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console
+                .WriteLine("L'aire est de : " +
+                CalculateRectangleArea(length: length, height: height));
+        }
+
+        /// <summary>
+        /// Calculates the rectangle's area
+        /// </summary>
+        /// <param name="length"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        private static string CalculateRectangleArea(int length, int height)
+        {            
+            var area = length * height;
+            StringBuilder sb = new StringBuilder(""+area);
+            return sb.ToString();
+        }
+    }
+}
