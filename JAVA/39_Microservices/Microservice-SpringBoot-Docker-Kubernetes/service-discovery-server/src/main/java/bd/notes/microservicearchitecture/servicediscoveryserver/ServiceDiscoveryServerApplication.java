32dcc4278d56125bcaae30d233385ece572589f3
Brique Deckard <pierre.4ntoine@outlook.fr>
2021-10-25 20:34:20 +0200

    added service discovery server

--- JAVA/39_Microservices/Microservice-SpringBoot-Docker-Kubernetes/service-discovery-server/src/main/java/bd/notes/microservicearchitecture/servicediscoveryserver/ServiceDiscoveryServerApplication.java
+++ JAVA/39_Microservices/Microservice-SpringBoot-Docker-Kubernetes/service-discovery-server/src/main/java/bd/notes/microservicearchitecture/servicediscoveryserver/ServiceDiscoveryServerApplication.java
@@ -2,12 +2,23 @@ package bd.notes.microservicearchitecture.servicediscoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+// This application is an Eureka Server
+// --> @EnableEurekaServer
+/*
+    Eureka Server is an application that holds the information about all
+    client-service applications. Every Micro service will register into
+    the Eureka server and Eureka server knows all the client applications
+    running on each port and IP address. Eureka Server is also known
+    as Discovery Server.
+ */
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceDiscoveryServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceDiscoveryServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDiscoveryServerApplication.class, args);
+    }
 
 }
