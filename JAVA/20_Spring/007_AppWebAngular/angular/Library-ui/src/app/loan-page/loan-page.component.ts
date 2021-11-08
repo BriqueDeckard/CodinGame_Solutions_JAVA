822c3deb21223f3b6f2b8a3f37ba88b59c3220e8
Brique Deckard <pierre.4ntoine@outlook.fr>
2021-11-08 19:57:31 +0100

    added gitignore

---
+++ JAVA/20_Spring/007_AppWebAngular/angular/Library-ui/src/app/loan-page/loan-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loan-page',
+  templateUrl: './loan-page.component.html',
+  styleUrls: ['./loan-page.component.css']
+})
+export class LoanPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
