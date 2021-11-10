8266e08da2b3b9dbf68b8d8db37bf682eb563624
Brique Deckard <pierre.4ntoine@outlook.fr>
2021-11-10 11:25:55 +0100

    added product alerts

--- ANGULAR/00_GettingStarted/GettingStarted/src/app/product-alerts/product-alerts.component.ts
+++ ANGULAR/00_GettingStarted/GettingStarted/src/app/product-alerts/product-alerts.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+// Allows to get data from a parent component 
 import { Input } from '@angular/core';
+// Allows to Pass data to a parent component
+import { Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
-
+/**
+ * Child component of ProductListComponent
+ */
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
@@ -9,8 +14,16 @@ import { Product } from '../products';
 })
 export class ProductAlertsComponent implements OnInit {
 
+  /**
+   * Get data from the parent component.
+   */
   @Input() product!: Product;
 
+  /**
+   * Export data to the parent component.
+   */
+  @Output() notify = new EventEmitter();
+  
   constructor() { }
 
   ngOnInit(): void {
